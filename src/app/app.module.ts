7d30a5d24dd7a7c390c5d8b5c9bbe763bee6b49a
ericmbouwe <ericmbouwe@yahoo.fr>
2021-12-14 19:55:39 +0100

    add material ui modules and shell component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSnackBarModule } from '@angular/material/snack-bar'
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
