b222b329a1167e303c23c23371ca4b351a2ddc41
ericmbouwe <ericmbouwe@yahoo.fr>
2021-12-17 01:15:05 +0100

    add seo service

--- src/app/articles/articles-routing.module.ts
+++ src/app/articles/articles-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { ArticlesListComponent } from './articles-list/articles-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ArticlesListComponent},
+  {path: ':id', component: ArticleDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
