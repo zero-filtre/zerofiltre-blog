39f234b581a977fe62e38c4d3456d675de1b2dbd
ericmbouwe <ericmbouwe@yahoo.fr>
2021-12-17 01:27:22 +0100

    remove the purge attribute from tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,7 @@ process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
 module.exports = {
   prefix: "",
   mode: "jit",
-  purge: {
-    content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
-  },
+  content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
