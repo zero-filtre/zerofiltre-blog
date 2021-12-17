96e355eb9533dbf061fa4dd0b0870fea8637c8e7
ericmbouwe <ericmbouwe@yahoo.fr>
2021-12-17 01:40:08 +0100

    articles routing path in the app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  {
+    path: 'articles',
+    loadChildren: () =>
+      import('./articles/articles.module').then(m => m.ArticlesModule),
+  }
 ];
 
 @NgModule({
