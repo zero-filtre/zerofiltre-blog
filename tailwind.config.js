eafc95a82164044838185065082112998bf32941
ericmbouwe <ericmbouwe@yahoo.fr>
2021-12-25 11:04:14 +0100

    add line clamp to article card desc in list page

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,5 +43,6 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
   ],
 };
