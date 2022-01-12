c41558deab031c6ba7ae4b84903d8aacdcc17324
ericmbouwe <ericmbouwe@yahoo.fr>
2022-01-12 20:00:31 +0100

    add lines numbers to code snippets

--- src/app/articles/article-entry-edit/article-entry-edit.component.ts
+++ src/app/articles/article-entry-edit/article-entry-edit.component.ts
@@ -11,98 +11,76 @@ export class ArticleEntryEditComponent implements OnInit {
 
   // @ViewChild('fileUpload', { static: false })
   // fileUpload!: ElementRef;
+  // public markdown!: string;
   public activeTab: string = 'editor';
 
-  form!: FormGroup;
+  public form!: FormGroup;
 
-  markdown = `## Markdown __rulez__!
-  ---
+  markdown = `
+    ## Markdown __rulez__!
+    # Jo
+    ---
 
-  ### Syntax highlight
-  \`\`\`ts
-  const language = 'typescript';
-  \`\`\`
+    ### Syntax highlight
+    \`\`\`ts
+      const language = 'typescript';
+    \`\`\`
 
-  ### Ordered Lists
-  1. bullet point
-  2. Another bullet point
+    ### Ordered Lists
+    1. bullet point
+    2. Another bullet point
 
-  ### Unordered list
-  - Unordered bullet
-  - Another unordered bullet
+    ### Unordered list
+    - Unordered bullet
+    - Another unordered bullet
 
-  ### Blockquote
-  > Blockquote to the max
+    ### Blockquote
+    > Blockquote to the max
 
-  \`\`\`js
-  const language = 'javascript';
-  \`\`\`
+    \`\`\`js
+      const language = 'javascript';
+    \`\`\`
 
-  \`\`\`java
-  String language = "java";
-  \`\`\`
+    \`\`\`java
+      String language = "java";
+    \`\`\`
 
-  ### Inline code
- \`Hello\`
+    ### Inline code
+   \`Inline code\`
 
-  ### Links
-  [link_text](https://google.com)
+    ### Links
+    [google-link](https://google.com)
 
-  ### Image
-  ![Screenshot from 2021-11-04 09-32-25.png](https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300)
+    ### Image
+    ![Screenshot from 2021-11-04 09-32-25.png](https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300)
 
-  ### Embed Video
-  [![Alternate Text]({image-url})]({video-url} "Link Title")
-  [![Alternate Text](https://img.youtube.com/watch?v=J1BDEl9uS18.jpg)](https://www.youtube.com/watch?v=J1BDEl9uS18)
+    ### Embed Video
+    [![]({image-url})]({video-url} "Link Title")
 
-  <figure class="video_container">
-  <video controls="true" allowfullscreen="true" poster="path/to/poster_image.png">
-    <source src="https://www.youtube.com/watch?v=J1BDEl9uS18" type="video/mp4">
-    <source src="https://www.youtube.com/watch?v=J1BDEl9uS18" type="video/ogg">
-    <source src="https://www.youtube.com/watch?v=J1BDEl9uS18" type="video/webm">
-  </video>
-  </figure>
+    ### Embed Iframe
 
-  ### Embed Iframe
-  
-  <figure class="video_container">
-  <iframe src="https://www.youtube.com/embed/J1BDEl9uS18" frameborder="0" allowfullscreen="true"></iframe>
-  </figure>
 
-  ### Divider
-  ---
+    ### Divider
+    ---
 
-  ### Inline HTML
-  <dl>
-  <dt>Definition list</dt>
-  <dd>Is something people use sometimes.</dd>
+    ### Inline HTML
+    <dl>
+    <dt>Definition list</dt>
+    <dd>Is something people use sometimes.</dd>
 
-  <dt>Markdown in HTML</dt>
-  <dd>Does *not* work **very** well. Use HTML <em>tags</em>.</dd>
-  </dl>
+    <dt>Markdown in HTML</dt>
+    <dd>Does *not* work **very** well. Use HTML <em>tags</em>.</dd>
+    </dl>
 
-  This is a regular paragraph.
 
-  <table>
-  <tr>
-  <td>Foo</td>
-  </tr>
-  </table>
+    ### Gipgy
 
-  This is another regular paragraph.
+    ### Mention a person
 
-  ### Gipgy
-  {% giphy https://giphy.com/embed/pynZagVcYxVUk %}
+    ### Embed CodePen
 
-  ### Mention a person
-  @ericmbouwe
-
-  ### Embed CodePen
-  {% codepen https://codepen.io/mehraddev/pen/LYpwLVq %}
-
-  ### Embed CodeSandBox
-  {% codesandbox https://codesandbox.io/s/rough-wildflower-bfh4z?file=/src/components/App.js %}
-  `;
+    ### Embed CodeSandBox
+    `;
 
   constructor(
     // private formuilder: FormBuilder,
