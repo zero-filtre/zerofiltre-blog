b99df459a13b5ecdd90616c796667754b705fee8
ericmbouwe <ericmbouwe@yahoo.fr>
2022-01-16 08:31:34 +0100

    update editor preview background

--- src/app/articles/article-entry-create/article-entry-create.component.ts
+++ src/app/articles/article-entry-create/article-entry-create.component.ts
@@ -17,8 +17,6 @@ export class ArticleEntryCreateComponent implements OnInit {
 
   markdown = `
     ## Markdown __rulez__!
-    # Jo
-    ---
 
     ### Syntax highlight
     \`\`\`ts
@@ -53,42 +51,12 @@ export class ArticleEntryCreateComponent implements OnInit {
     ### Image
     ![Screenshot from 2021-11-04 09-32-25.png](https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300)
 
-    ### Embed Video
-    [![]({image-url})]({video-url} "Link Title")
-
     ### Embed Iframe
-    <div>
-    <iframe id="inlineFrameExample"
-    title="Inline Frame Example"
-    width="300"
-    height="200"
-    src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+    <iframe src="https://www.youtube.com/embed/yz8x71BiGXg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
     </iframe>
-    </div>
 
     ### Divider
     ---
-
-  <iframe src="https://www.youtube.com/embed/yz8x71BiGXg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-  </iframe>
-
-    ### Inline HTML
-    <dl>
-    <dt>Definition list</dt>
-    <dd>Is something people use sometimes.</dd>
-
-    <dt>Markdown in HTML</dt>
-    <dd>Does *not* work **very** well. Use HTML <em>tags</em>.</dd>
-    </dl>
-
-
-    ### Gipgy
-
-    ### Mention a person
-
-    ### Embed CodePen
-
-    ### Embed CodeSandBox
     `;
 
   constructor(
