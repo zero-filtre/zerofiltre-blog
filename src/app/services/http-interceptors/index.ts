799714c22a9a10fbaf8445b34fab3e22c1b56e31
ericmbouwe <ericmbouwe@yahoo.fr>
2022-01-25 11:16:26 +0100

    change name of error interceptor

--- src/app/services/http-interceptors/index.ts
+++ src/app/services/http-interceptors/index.ts
@@ -1,6 +1,6 @@
 /* "Barrel" of Http Interceptors */
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpErrorInterceptor } from './article.interceptor'
+import { HttpErrorInterceptor } from './http-error.interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
