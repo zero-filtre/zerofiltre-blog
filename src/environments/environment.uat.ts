b3a1278c42ea4438a9753c0922b51c9328354bd2
ericmbouwe <ericmbouwe@yahoo.fr>
2022-02-23 17:32:44 +0100

    Update github clinet id for uat env

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://blog-api-uat.zerofiltre.tech',
   blogUrl: 'https://blog-uat.zerofiltre.tech',
-  GITHUB_CLIENT_ID: '9b6bffa9841d19dfd8aa',
+  GITHUB_CLIENT_ID: 'dee4a78da7c2c935ae27',
   STACK_OVERFLOW_CLIENT_ID: '22742',
   gitHubRedirectURL: 'https://blog-uat.zerofiltre.tech/user/social-auth',
   stackOverflowRedirectURL: 'https://blog-uat.zerofiltre.tech/user/social-auth',
