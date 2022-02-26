ac9354428b6b782099912743754cc504d3b26586
ericmbouwe <ericmbouwe@yahoo.fr>
2022-02-26 15:35:57 +0100

    Add tailwind css official lib

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts,css}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserat: "'Montserrat', sans-serif",
+      },
+      colors: {
+        primary: {
+          500: "#15b2bc",
+          300: "#00c2cb",
+          100: "#f2feff",
+        },
+        secondary: "#052b5d",
+        accent: "#fdbc5a",
+        error: "#f55f44",
+        grays: "#eef1f4",
+        skin: {
+          base: "#6b7280",
+          link: "#f3f4f6",
+          muted: "#9ca3af",
+          inverted: "#052b5d",
+          border: "#e5e7eb",
+          card: "#fff",
+          cardMuted: "#f9fafb",
+          borderBold: "#d1d5db",
+          bg: "#f9fafb"
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
