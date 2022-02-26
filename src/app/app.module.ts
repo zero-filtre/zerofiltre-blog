0f4d39a3bd8e9c51f818f3fe4137b6029093bfa3
ericmbouwe <ericmbouwe@yahoo.fr>
2022-02-26 22:57:53 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import { LOCALE_ID } from '@angular/core';
 
+import { httpInterceptorProviders } from './services/http-interceptors';
+
 registerLocaleData(localeFr, 'fr');
 
 @NgModule({
@@ -28,6 +30,7 @@ registerLocaleData(localeFr, 'fr');
     AppRoutingModule,
   ],
   providers: [
+    // httpInterceptorProviders,
     { provide: LOCALE_ID, useValue: "fr-FR" },
   ],
   bootstrap: [AppComponent]
