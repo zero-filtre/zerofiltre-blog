67540ca405a49a63cdc5fba39c8a86407eb70eb3
ericmbouwe <ericmbouwe@yahoo.fr>
2022-03-05 10:12:15 +0100

    Remove duplicated Reactive form module from user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomePageComponent } from './home-page/home-page.component';
@@ -14,8 +14,8 @@ import localeFr from '@angular/common/locales/fr';
 import { LOCALE_ID } from '@angular/core';
 
 import { httpInterceptorProviders } from './services/http-interceptors';
-
 import { JWT_OPTIONS, JwtInterceptor, JwtModule } from '@auth0/angular-jwt';
+
 import { environment } from 'src/environments/environment';
 import { AuthService } from './user/auth.service';
 
@@ -41,7 +41,6 @@ function jwtOptionsFactory(authService: AuthService) {
     MarkdownModule.forRoot(),
     SharedModule,
     AppRoutingModule,
-    // BrowserTransferStateModule,
 
     JwtModule.forRoot({
       jwtOptionsProvider: {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { PasswordResetPageComponent } from './password-reset-page/password-reset-page.component';
 import { PasswordRenewalPageComponent } from './password-renewal-page/password-renewal-page.component';
@@ -27,7 +26,6 @@ import { SocialAuthComponent } from './social-auth/social-auth.component';
     CommonModule,
     SharedModule,
     UserRoutingModule,
-    ReactiveFormsModule
   ]
 })
 export class UserModule { }
