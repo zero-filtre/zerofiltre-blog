f493db7f3719ad1a21497c1a94c77c46e7bb88f8
ericmbouwe <ericmbouwe@yahoo.fr>
2022-03-08 01:59:56 +0100

    Update content value for auto save after loading an image in the editor

--- src/app/articles/article-entry-create/article-entry-create.component.ts
+++ src/app/articles/article-entry-create/article-entry-create.component.ts
@@ -209,7 +209,7 @@ export class ArticleEntryCreateComponent implements OnInit {
     if (host === 'editorImage') {
       this.insertAtCursor(content, imgSrcValue);
       this.form.patchValue({ content: content?.value });
-      this.EditorText$.next(this.content?.value);
+      this.EditorText$.next(content?.value);
     }
 
     // this.fileUploadService.uploadImage(formData).pipe(
