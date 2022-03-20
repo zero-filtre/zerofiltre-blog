9f147049fce9034c0ea96c2fa248994cca38a01c
ericmbouwe <ericmbouwe@yahoo.fr>
2022-03-20 18:43:39 +0100

    Check if in browser before using Prism

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { map, Observable, shareReplay } from 'rxjs';
 import { MessageService } from './services/message.service';
@@ -21,21 +22,25 @@ export class AppComponent implements OnInit {
       shareReplay()
     );
 
-  lang = (window.navigator as any).language
 
   constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
     private translate: TranslateService,
     private breakpointObserver: BreakpointObserver,
     private messageService: MessageService,
-    public authService: AuthService
+    public authService: AuthService,
   ) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('fr');
+    this.translate.setDefaultLang('fr');
+    this.setBrowserTranslationConfigs();
+  }
 
-    // the language to use, if the lang isn't available, it will use the current loader to get them
-    translate.use(this.lang);
+  setBrowserTranslationConfigs() {
+    if (isPlatformBrowser(this.platformId)) {
+      const browserLanguage = (window.navigator as any).language
 
-    console.log('LOCALE: ', this.lang);
+      this.translate.use(browserLanguage);
+      console.log('LOCALE: ', browserLanguage);
+    }
   }
 
   public logout() {
@@ -54,16 +59,18 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    Prism.plugins.toolbar.registerButton('select-code', function (_env: any) {
-      const button = document.createElement('button');
+    if (isPlatformBrowser(this.platformId)) {
+      Prism.plugins.toolbar.registerButton('select-code', function (_env: any) {
+        const button = document.createElement('button');
 
-      button.innerHTML = `
+        button.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
       <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
       </svg>`;
 
-      return button;
-    });
+        return button;
+      });
+    }
 
     // const copyBtn = (document as any).querySelector('.copy-to-clipboard-button');
     // copyBtn.onclick = this.alertCopy();
