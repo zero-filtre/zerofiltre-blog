bbd8ade2a17252c70759a58df5027061744f7e3d
ericmbouwe <ericmbouwe@yahoo.fr>
2022-04-04 08:57:12 +0100

    Update some profile styles

--- src/app/user/profile-entry-edit/profile-entry-edit.component.ts
+++ src/app/user/profile-entry-edit/profile-entry-edit.component.ts
@@ -33,23 +33,32 @@ export class ProfileEntryEditComponent implements OnInit {
       bio: [this.user?.bio],
       website: [this.user?.website],
       pseudo: [this.user?.pseudoName],
-      socialLinks: this.fb.array([
-        this.fb.group({
-          platform: ['TWITTER'],
-          link: [this.user?.socialLinks.find((profile: any) => profile.platform === 'TWITTER')?.link],
-          userId: [this.user?.id]
-        }),
-        this.fb.group({
-          platform: ['GITHUB'],
-          link: [this.user?.socialLinks.find((profile: any) => profile.platform === 'GITHUB')?.link],
-          userId: [this.user?.id]
-        }),
-        this.fb.group({
-          platform: ['STACKOVERFLOW'],
-          link: [this.user?.socialLinks.find((profile: any) => profile.platform === 'STACKOVERFLOW')?.link],
-          userId: [this.user?.id]
-        }),
-      ])
+
+      // socialLinks: this.fb.array(
+      // [
+      //   this.fb.group({
+      //     platform: ['TWITTER'],
+      //     link: [this.user?.socialLinks.find((profile: any) => profile.platform === 'TWITTER')?.link],
+      //     userId: [this.user?.id]
+      //   }),
+      //   this.fb.group({
+      //     platform: ['GITHUB'],
+      //     link: [this.user?.socialLinks.find((profile: any) => profile.platform === 'GITHUB')?.link],
+      //     userId: [this.user?.id]
+      //   }),
+      //   this.fb.group({
+      //     platform: ['STACKOVERFLOW'],
+      //     link: [this.user?.socialLinks.find((profile: any) => profile.platform === 'STACKOVERFLOW')?.link],
+      //     userId: [this.user?.id]
+      //   }),
+      // ]
+      // []
+      // )
+
+      twitterLink: [this.user?.socialLinks.find((profile: any) => profile.platform === 'TWITTER')?.link],
+      githubLink: [this.user?.socialLinks.find((profile: any) => profile.platform === 'GITHUB')?.link],
+      soLink: [this.user?.socialLinks.find((profile: any) => profile.platform === 'STACKOVERFLOW')?.link],
+      socialLinks: [[]]
     })
   }
 
@@ -58,16 +67,77 @@ export class ProfileEntryEditComponent implements OnInit {
   get fullName() { return this.form.get('fullName'); }
   get bio() { return this.form.get('bio'); }
   get website() { return this.form.get('website'); }
-  get socialLinks() { return this.form.get('socialLinks') as FormArray }
+  get twitterLink() { return this.form.get('twitterLink'); }
+  get githubLink() { return this.form.get('githubLink'); }
+  get soLink() { return this.form.get('soLink'); }
+  get socialLinks() { return this.form.get('socialLinks')! }
+  // get socialLinks() { return this.form.get('socialLinks') as FormArray }
 
-  public setFormUserInfos() {
-    this.setUserSocialLinks();
+  // public addSocialLink(obj: any) {
+  //   this.socialLinks.push(
+  //     this.fb.group({
+  //       platform: [obj.platform],
+  //       link: [obj.link],
+  //       userId: [obj.userId]
+  //     })
+  //   );
+  // }
+
+  // public removeSocialLink(id: any) {
+  //   this.socialLinks.removeAt(id)
+  // }
+
+  // public getUserSocialLinks(): any {
+  //   this.user?.socialLinks.forEach((sl: any) => {
+  //     if (sl.link) this.addSocialLink(sl)
+  //   })
+  // }
+
+  public setUserSocialLinksBeforeUpdateRequest() {
+    const socialLinks = this.socialLinks.value
+    let realSocialLinksValue;
+
+    // socialLinks.forEach((sl: any, id: any) => {
+    //   if (!sl.link) this.removeSocialLink(id)
+    // })
+
+    // realSocialLinksValue = socialLinks.filter((sl: any) => sl.link)
+
+    const links = [this.twitterLink, this.githubLink, this.soLink]
+    const linksToSend = links.filter((li: any) => li.value).map((li: any) => {
+      let obj;
+      obj = {
+        platform: 'plat',
+        link: li.value,
+        userId: this.user?.id
+      }
+      return obj;
+    })
+
+    // this.form.removeControl('twitterLink')
+    // this.form.removeControl('githubLink')
+    // this.form.removeControl('soLink')
+
+    this.form.patchValue({ socialLinks: linksToSend })
   }
 
-  public setUserSocialLinks(): any {
-    this.socialLinks.controls.forEach((formGroup: any, id: any) => {
-      // formGroup.controls['link'].setValue('the user link');
-    });
+  public updateUserInfos(): void {
+    this.loading = true;
+    this.setUserSocialLinksBeforeUpdateRequest();
+
+    console.log('FORM VALUE: ', this.form.value);
+    // return
+
+    this.authService.updateUserProfile(this.form.value).subscribe({
+      next: (response: User) => {
+        this.loading = false;
+        this.messageService.openSnackBarSuccess('Enregistrement reussi !', 'OK', 0);
+        this.authService.setUserData(response)
+      },
+      error: (_error: HttpErrorResponse) => {
+        this.loading = false;
+      }
+    })
   }
 
   public getUserProfile(): void {
@@ -78,22 +148,7 @@ export class ProfileEntryEditComponent implements OnInit {
         this.profession?.setValue(this.user.profession)
         this.socialLinks?.setValue(this.user.socialLinks)
       },
-      error: (error: HttpErrorResponse) => {
-      }
-    })
-  }
-
-  public updateUserInfos(): void {
-    this.loading = true;
-
-    this.authService.updateUserProfile(this.form.value).subscribe({
-      next: (response: User) => {
-        this.loading = false;
-        this.messageService.openSnackBarSuccess('Enregistrement reussi !', 'Ok', 0);
-        this.authService.setUserData(response)
-      },
-      error: (error: HttpErrorResponse) => {
-        this.loading = false;
+      error: (_error: HttpErrorResponse) => {
       }
     })
   }
@@ -102,7 +157,8 @@ export class ProfileEntryEditComponent implements OnInit {
     this.user = this.authService?.currentUsr
 
     this.InitForm();
-    this.setFormUserInfos();
+
+    // this.getUserSocialLinks();
 
     this.seo.generateTags({
       title: "Modifier son profil | Zerofiltre.tech",
