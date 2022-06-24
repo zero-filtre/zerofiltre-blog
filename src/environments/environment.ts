95369659055c7be9a184e8e1f752c8a2f8818508
geek3000 <jonathantchuente@gmail.com>
2022-06-24 06:19:49 +0100

    env injection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = JSON.parse(localStorage.getItem("env-value") || '')
+
+let env = {}
+
+if(typeof localStorage !== 'undefined'){
+    env = JSON.parse(localStorage.getItem("env-value") || '')
+}
+
+
+
+export const environment:any = env;
