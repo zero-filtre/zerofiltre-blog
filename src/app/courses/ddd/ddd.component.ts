e7bf51735997df1de2b79f0b1c45533261a66ad3
ericmbouwe <ericmbouwe@yahoo.fr>
2022-07-01 10:27:28 +0100

    Adjust ddd page master class section's styles

--- src/app/courses/ddd/ddd.component.ts
+++ src/app/courses/ddd/ddd.component.ts
@@ -7,14 +7,12 @@ import { SeoService } from 'src/app/services/seo.service';
   styleUrls: ['./ddd.component.css']
 })
 export class DddComponent implements OnInit, OnDestroy {
-  // public whySectionImage = 'https://ik.imagekit.io/lfegvix1p/domain-bgg-off_rKGMAzNWa.png';
   public whySectionImage = 'https://ik.imagekit.io/lfegvix1p/inspiration__1__x-J3qi03A.svg';
   public learnSectionImage = 'https://ik.imagekit.io/lfegvix1p/Meza-3_a9TUXnmba.svg';
   public stepSectionImage = 'https://ik.imagekit.io/lfegvix1p/a_jWE-ysuJM.svg';
   public practiceSectionImage = 'https://ik.imagekit.io/lfegvix1p/Mesa-1_rxodYJjzE.svg';
-  public masterSectionImage = 'https://ik.imagekit.io/lfegvix1p/inspiration__1__x-J3qi03A.svg';
+  public masterSectionImage = 'https://ik.imagekit.io/lfegvix1p/inspiration_EkvzNOIP2U.svg';
 
-  // public lessonGroupImage1 = 'https://ik.imagekit.io/lfegvix1p/ddd-icon-bg-off_uYBd7RKWP.png';
   public lessonGroupImage1 = 'https://ik.imagekit.io/lfegvix1p/presentation_1_-REzJE-9c.svg';
   public lessonGroupImage2 = 'https://ik.imagekit.io/lfegvix1p/presentation_1_-REzJE-9c.svg';
   public lessonGroupImage3 = 'https://ik.imagekit.io/lfegvix1p/presentation_1_-REzJE-9c.svg';
