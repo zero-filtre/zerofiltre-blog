a2f17518c45807f9c24fb3cf0a0ff631d1e4a99b
eric <ericmbouwe@yahoo.fr>
2022-08-09 12:03:30 +0100

    Fix missing comma in package json file + missing imports + list issue

--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -86,7 +86,7 @@ export function nFormatter(num: number): string {
   if (num >= 1000) {
     return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'k';
   }
-  return num.toString();
+  return num?.toString();
 }
 
 export function AddTargetToExternalLinks(): void {
