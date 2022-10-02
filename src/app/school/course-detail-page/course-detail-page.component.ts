02d820ae3ccc39091aba4c5ebc55d2d0b1dd6ac0
eric <ericmbouwe@yahoo.fr>
2022-10-02 12:16:51 +0100

    Add curriculum desktop and mobile styles

--- src/app/school/course-detail-page/course-detail-page.component.ts
+++ src/app/school/course-detail-page/course-detail-page.component.ts
@@ -18,6 +18,58 @@ export class CourseDetailPageComponent implements OnInit {
   public lessonGroupImage2 = 'https://ik.imagekit.io/lfegvix1p/presentation_1_-REzJE-9c.svg';
   public lessonGroupImage3 = 'https://ik.imagekit.io/lfegvix1p/presentation_1_-REzJE-9c.svg';
 
+  course: any = {
+    name: 'Apprenez le DDD',
+    chapters: [
+      {
+        title: 'Titre du chapitre 1',
+        lessons: [
+          {
+            type: 'video',
+            name: 'lesson numero 1',
+            duration: '3:15',
+            free: true
+          },
+          {
+            type: 'video',
+            name: 'lesson numero 2',
+            duration: '1:15',
+            free: false
+          },
+          {
+            type: 'doc',
+            name: 'lesson numero 3',
+            duration: '0:15',
+            free: true
+          },
+        ]
+      },
+      {
+        title: 'Titre du chapitre 2',
+        lessons: [
+          {
+            type: 'video',
+            name: 'lesson numero 1',
+            duration: '3:15',
+            free: true
+          },
+          {
+            type: 'doc',
+            name: 'lesson numero 2',
+            duration: '1:15',
+            free: false
+          },
+          {
+            type: 'video',
+            name: 'lesson numero 3',
+            duration: '0:15',
+            free: false
+          },
+        ]
+      }
+    ]
+  }
+
   constructor(
     private seo: SeoService
   ) { }
