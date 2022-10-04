20f10f1573b7589915f876e62a756e8c16734231
eric <ericmbouwe@yahoo.fr>
2022-10-04 14:24:54 +0100

    Transform curriculum into a single component

--- src/app/school/school.module.ts
+++ src/app/school/school.module.ts
@@ -5,14 +5,14 @@ import { SchoolRoutingModule } from './school-routing.module';
 import { CourseListPageComponent } from './course-list-page/course-list-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { CourseDetailPageComponent } from './course-detail-page/course-detail-page.component';
-import { CurriculumSidebarComponent } from './curriculum-sidebar/curriculum-sidebar.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
 
 
 @NgModule({
   declarations: [
     CourseListPageComponent,
     CourseDetailPageComponent,
-    CurriculumSidebarComponent
+    CurriculumComponent,
   ],
   imports: [
     CommonModule,
