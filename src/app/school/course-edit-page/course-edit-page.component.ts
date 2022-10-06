9533eb97c6053ae2e72348786a95ff90e8da3327
Eric Mbouwe <ericmbouwe@yahoo.fr>
2022-10-06 12:49:36 +0100

    Add course edit page Ui (#53)

--- src/app/school/course-edit-page/course-edit-page.component.ts
+++ src/app/school/course-edit-page/course-edit-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-edit-page',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseEditPageComponent implements OnInit {
 
-  constructor() { }
+  public activeTab: string = 'editor';
+
+  course = new FormGroup({
+    title: new FormControl(''),
+    videoId: new FormControl('355926790'),
+    content: new FormControl('')
+  });
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+
+  public setActiveTab(tabName: string): void {
+    if (tabName === 'editor') this.activeTab = 'editor'
+    if (tabName === 'preview') this.activeTab = 'preview'
+    if (tabName === 'help') this.activeTab = 'help'
+  }
+
+  public handleCreateCourse() {
+    this.router.navigate(["/cours/5"])
+  }
+
 
 }
