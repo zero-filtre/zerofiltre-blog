51583526c98bbc7e9244b44432c8e4849234ae85
eric <ericmbouwe@yahoo.fr>
2022-10-06 01:06:15 +0100

    Add into lesson page action buttons and popups to create a new chapter and lesson

--- src/app/school/curriculum-sidebar/curriculum-sidebar.component.ts
+++ src/app/school/curriculum-sidebar/curriculum-sidebar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AuthService } from '../../user/auth.service';
+import { ChapterInitPopupComponent } from '../chapter-init-popup/chapter-init-popup.component';
+import { ChapterDeletePopupComponent } from '../chapter-delete-popup/chapter-delete-popup.component';
+import { LessonInitPopupComponent } from '../lesson-init-popup/lesson-init-popup.component';
+import { LessonDeletePopupComponent } from '../lesson-delete-popup/lesson-delete-popup.component';
 
 @Component({
   selector: 'app-curriculum-sidebar',
@@ -7,7 +14,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurriculumSidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public dialogNewChapterRef: MatDialog,
+    private dialogDeleteChapterRef: MatDialog,
+    private dialogNewLessonRef: MatDialog,
+    private dialogDeleteLessonRef: MatDialog,
+    private router: Router
+  ) { }
+
+  isAuthor(user: any, cours: any): boolean {
+    return user?.id === cours?.author?.id
+  }
 
   course: any = {
     name: 'Exemple de titre du cours, Apprenez le DDD',
@@ -128,6 +146,42 @@ export class CurriculumSidebarComponent implements OnInit {
         ]
       }
     ]
+  }
+
+  openChapterInitDialog(): void {
+    this.dialogNewChapterRef.open(ChapterInitPopupComponent, {
+      width: '850px',
+      height: '350px',
+      panelClass: 'article-popup-panel',
+    });
+  }
+
+  openChapterDeleteDialog(chapterId: number | undefined): void {
+    this.dialogDeleteChapterRef.open(ChapterDeletePopupComponent, {
+      panelClass: 'delete-article-popup-panel',
+      data: {
+        id: chapterId,
+        history: this.router.url
+      }
+    });
+  }
+
+  openLessonInitDialog(): void {
+    this.dialogNewLessonRef.open(LessonInitPopupComponent, {
+      width: '850px',
+      height: '350px',
+      panelClass: 'article-popup-panel',
+    });
+  }
+
+  openLessonDeleteDialog(lessonId: number | undefined): void {
+    this.dialogDeleteLessonRef.open(LessonDeletePopupComponent, {
+      panelClass: 'delete-article-popup-panel',
+      data: {
+        id: lessonId,
+        history: this.router.url
+      }
+    });
   }
 
   ngOnInit(): void {
