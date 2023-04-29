e381304060b182b855cad618d642d1a2a6fb993e
eric <ericmbouwe@yahoo.fr>
2023-04-30 00:16:17 +0100

    Mise à jour des dependance du projet

--- src/app/shared/image/image.component.ts
+++ src/app/shared/image/image.component.ts
@@ -15,7 +15,7 @@ export class ImageComponent implements OnInit {
   imageKitAccountId = 'lfegvix1p'
 
   defaultImage = 'Article_default_oz_Yb-VZj.svg'
-  srcsetValue !: string;
+  srcsetValue!: string;
 
   browserRunning!: boolean;
   serverRunning!: boolean;
@@ -43,6 +43,13 @@ export class ImageComponent implements OnInit {
         https://ik.imagekit.io/${this.imageKitAccountId}/tr:w-1200,ar-auto,dpr-auto,di-${this.defaultImage}/${imageName} 1400w`;
       } else {
         src = this.sourceUrl
+
+        this.srcsetValue = `https://ik.imagekit.io/${this.imageKitAccountId}/tr:w-400,ar-auto,dpr-auto,di-${this.defaultImage}/${imageName} 400w,
+        https://ik.imagekit.io/${this.imageKitAccountId}/tr:w-400,ar-auto,dpr-auto,di-${this.defaultImage}/${imageName} 800w,
+        https://ik.imagekit.io/${this.imageKitAccountId}/tr:w-600,ar-auto,dpr-auto,di-${this.defaultImage}/${imageName} 1200w,
+        https://ik.imagekit.io/${this.imageKitAccountId}/tr:w-800,ar-auto,dpr-auto,di-${this.defaultImage}/${imageName} 1300w,
+        https://ik.imagekit.io/${this.imageKitAccountId}/tr:w-1200,ar-auto,dpr-auto,di-${this.defaultImage}/${imageName} 1400w`;
+
       }
     } else {
       src = imageKitBaseUrl + 'not_found_image.jpg';
