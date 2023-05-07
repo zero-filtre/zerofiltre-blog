5b5be562705d7aab513eadbe51bfedd1e68c22a2
Eric Mbouwe <ericmbouwe@yahoo.fr>
2023-05-07 10:03:04 +0100

    Fetch recent courses in the footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { LoadEnvService } from 'src/app/services/load-env.service';
 import { capitalizeString, getCurrentYear } from 'src/app/services/utilities.service';
 import { environment } from 'src/environments/environment';
 import { SeoService } from 'src/app/services/seo.service';
+import { Course } from 'src/app/school/courses/course';
+import { CourseService } from 'src/app/school/courses/course.service';
 
 @Component({
   selector: 'app-footer',
@@ -16,13 +18,19 @@ import { SeoService } from 'src/app/services/seo.service';
 export class FooterComponent implements OnInit {
   currentYear!: number;
   recentArticles$!: Observable<Article[]>;
+  recentCourses$!: Observable<Course[]>;
   PUBLISHED = 'published';
 
   readonly servicesUrl = environment.servicesUrl;
+  readonly blogUrl = environment.blogUrl;
+  readonly activeCourseModule = environment.courseRoutesActive === 'true';
+
+  prod = this.blogUrl.startsWith('https://dev.') ? false : true;
 
   constructor(
     private loadEnvService: LoadEnvService,
     private articleService: ArticleService,
+    private courseService: CourseService,
     private seo: SeoService,
     @Inject(PLATFORM_ID) private platformID: any
   ) { }
@@ -50,12 +58,24 @@ export class FooterComponent implements OnInit {
       )
   }
 
+  fetchRecentCpourses() {
+    this.recentCourses$ = this.courseService
+      .fetchAllCoursesByFilter(0, 5, this.PUBLISHED)
+      .pipe(
+        map(({ content }: any) => {
+          return content;
+        }),
+        // tap(console.log)
+      )
+  }
+
   ngOnInit(): void {
     this.isMounted();
-    this.loadCurrentYear();
-
+    
     if (isPlatformBrowser(this.platformID)) {
+      this.loadCurrentYear();
       this.fetchRecentArticles();
+      this.fetchRecentCpourses();
     }
     
   }
