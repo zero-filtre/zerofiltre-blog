bef60b167659eda87d9e005dbe5ce7ce06bdd3da
eric <ericmbouwe@yahoo.fr>
2023-05-26 21:58:23 +0100

    Ajout boutton Pro à la navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadEnvService } from 'src/app/services/load-env.service';
+import { MessageService } from 'src/app/services/message.service';
+import { PaymentService } from 'src/app/services/payment.service';
 import { SeoService } from 'src/app/services/seo.service';
 import { AuthService } from 'src/app/user/auth.service';
+import { User } from 'src/app/user/user.model';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -27,12 +30,39 @@ export class HeaderComponent implements OnInit {
     private loadEnvService: LoadEnvService,
     public authService: AuthService,
     private router: Router,
-    public seo: SeoService
+    public seo: SeoService,
+    private messageService: MessageService,
+    private paymentService: PaymentService
   ) { }
 
   public logout() {
     this.authService.logout();
     this.router.navigateByUrl('/');
+  }
+
+  subscribeToPro() {
+
+    const currUser = this.authService.currentUsr as User;
+    const loggedIn = !!currUser;
+
+    if (!loggedIn) {
+      this.router.navigate(
+        ['/login'],
+        {
+          queryParams: { redirectURL: this.router.url },
+          queryParamsHandling: 'merge',
+        });
+
+      this.messageService.openSnackBarInfo('Veuillez vous connecter pour prendre votre abonnement PRO 🤗', 'OK');
+
+      return;
+    }
+
+    const payload = { productId: 1, productType: 'COURSE' }
+    const type = 'pro'
+
+    this.paymentService.openPaymentDialog(payload, type);
+
   }
 
   ngOnInit(): void {
