068207d6ff30f835a3265c3a9177884a64d29f6f
eric <ericmbouwe@yahoo.fr>
2023-07-08 00:45:18 +0100

    Can update user infos

--- src/app/shared/bot-user-infos/bot-user-infos.component.ts
+++ src/app/shared/bot-user-infos/bot-user-infos.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { resolve } from 'dns';
+import { catchError, throwError } from 'rxjs';
+import { BotService } from 'src/app/services/bot.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-bot-user-infos',
@@ -9,33 +13,65 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class BotUserInfosComponent {
   @Input() data!: any;
 
-  userInfoForm: FormGroup;
+  form: FormGroup;
   editMode = false;
+  saving: boolean;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private bot: BotService,
+    private notify: MessageService
     ) {
-    this.userInfoForm = this.fb.group({
+    this.form = this.fb.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required]
+      prename: [''],
+      phone: [''],
+      password: [''],
+      gender: [''],
+      city: [''],
+      statut: [''],
+      domain: [''],
     });
   }
+
+  get name() { return this.form.get('name'); }
+  get prename() { return this.form.get('prename'); }
+  get phone() { return this.form.get('phone'); }
+  get gender() { return this.form.get('gender'); }
+  get city() { return this.form.get('city'); }
+  get statut() { return this.form.get('statut'); }
+  get domain() { return this.form.get('domain'); }
 
   edit() {
     this.editMode = true;
 
-    this.userInfoForm.patchValue({
+    this.form.patchValue({
       name: this.data.name,
-      email: this.data.name,
-      phone: this.data.name
+      prename: this.data.prename,
+      phone: this.data.phone,
+      gender: this.data.gender,
+      city: this.data.city,
+      statut: this.data.statut,
+      domain: this.data.domain
     });
   }
 
   save() {
-    // Handle form submission and saving of data
-    // ...
-    this.editMode = false;
+    this.saving = true;
+    this.bot.updateUser(this.form.value)
+      .pipe(
+        catchError(err => {
+          this.saving = false;
+          this.notify.openSnackBarError(err.message, '');
+          return throwError(() => err?.message)
+        })
+      )
+      .subscribe(_response => {
+        this.saving = false;
+        this.data = this.form.value;
+        this.editMode = false;
+      })
+
   }
 
   cancel() {
