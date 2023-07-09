20e7d621133dce23d33f1ff1487ee4d190ca3221
eric <ericmbouwe@yahoo.fr>
2023-07-10 00:24:24 +0100

    Taking account of previous week data in the chart

--- src/app/shared/stat-chart/stat-chart.component.ts
+++ src/app/shared/stat-chart/stat-chart.component.ts
@@ -11,25 +11,32 @@ export class StatChartComponent {
   };
 
   chartLabels: string[] = [];
-  chartData: number[] = [];
+  chartDataPrevWeek: number[] = [];
+  chartDataCurrWeek: number[] = [];
 
   @Input() dataset!: any;
 
   constructor() { }
 
   initData() {
-    let dailyReports = [];
+    let dailyReportPrevWeek = [];
+    let dailyReportCurrWeek = [];
     const { prevWeek, currWeek } = this.dataset;
 
-    const pureData1 = this.parseData(currWeek)
-    const pureData2 = this.parseData(prevWeek)
+    const pureData1 = this.parseData(prevWeek)
+    const pureData2 = this.parseData(currWeek)
 
     pureData1.forEach((data: any) => {
-      dailyReports = [...dailyReports, { day: Object.keys(data)[0], quantity: Object.values(data)[0]}] 
+      dailyReportPrevWeek = [...dailyReportPrevWeek, { day: Object.keys(data)[0], quantity: Object.values(data)[0]}] 
     })
 
-    this.chartLabels = dailyReports.map(report => report.day);
-    this.chartData = dailyReports.map(report => report.quantity);
+    pureData2.forEach((data: any) => {
+      dailyReportCurrWeek = [...dailyReportCurrWeek, { day: Object.keys(data)[0], quantity: Object.values(data)[0] }]
+    })
+
+    this.chartLabels = dailyReportCurrWeek.map(report => report.day);
+    this.chartDataPrevWeek = dailyReportPrevWeek.map(report => report.quantity);
+    this.chartDataCurrWeek = dailyReportCurrWeek.map(report => report.quantity);
   }
 
   parseData(data: any) {
