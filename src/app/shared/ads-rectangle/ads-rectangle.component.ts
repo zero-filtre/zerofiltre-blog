2e40ba216a47b5a50587d96cbde42cb3b8d50d29
eric <ericmbouwe@yahoo.fr>
2024-02-19 17:41:04 +0100

    Add new sponsor content image

--- src/app/shared/ads-rectangle/ads-rectangle.component.ts
+++ src/app/shared/ads-rectangle/ads-rectangle.component.ts
@@ -12,16 +12,20 @@ export class AdsRectangleComponent {
 
   @Input() random: any;
 
+  defaultColor = 'border-primary-500 text-primary-500 group-hover:bg-primary-500'
+
   ads = [
     {
       url: '/cours/1',
       image: 'https://ik.imagekit.io/lfegvix1p/ddd-imagee_7A342RNOT.svg?updatedAt=1681558221642',
-      text: 'Apprenez et implémentez des modèles de conception simples pour satisfaire des besoins complexes'
+      text: 'Apprenez et implémentez des modèles de conception simples pour satisfaire des besoins complexes',
+      color: this.defaultColor
     },
     {
       url: '/cours/3',
-      image: 'https://ik.imagekit.io/lfegvix1p/ddd-imagee_7A342RNOT.svg?updatedAt=1681558221642',
-      text: "Restez compétitifs sur le marché du logiciel en devenant des devs qui 'RUN' ce qu'ils 'BUILD'"
+      image: 'https://ik.imagekit.io/lfegvix1p/annonce_build_and_run_elevez_le_dev_au_dela_du_build_k8fX8AYfF.png?updatedAt=1708359934013',
+      text: "Restez compétitifs sur le marché du logiciel en devenant des devs qui 'RUN' ce qu'ils 'BUILD'",
+      color: 'border-accent-400 text-accent-400 group-hover:bg-accent-400'
     }
   ]
 
