640b7139fc40ce133a29cc1d35ecc873a2d8076e
eric <ericmbouwe@yahoo.fr>
2024-04-27 17:19:23 +0100

    Adjust course list styles

--- src/app/shared/base-course-list/base-course-list.component.ts
+++ src/app/shared/base-course-list/base-course-list.component.ts
@@ -43,7 +43,7 @@ export class BaseCourseListComponent implements OnInit {
   scrollyPageNumber = 0;
 
   pageNumber: number = 0;
-  pageItemsLimit: number = 8;
+  pageItemsLimit: number = 9;
 
   loadingMore = false;
   loading = false;
