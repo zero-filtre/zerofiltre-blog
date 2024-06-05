1dde97f0a0c3ccfc06be1c3ed7a1ced8a90c0613
eric <ericmbouwe@yahoo.fr>
2024-06-05 14:00:12 +0100

    Add annoucement banner component

---
+++ src/app/shared/announcement-banner/announcement-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-banner',
+  templateUrl: './announcement-banner.component.html',
+  styleUrls: ['./announcement-banner.component.css'],
+})
+export class AnnouncementBannerComponent {
+  @Input() bannerText: string = 'This is an announcement!';
+  @Input() isBannerVisible: boolean = false;
+
+  closeBanner() {
+    this.isBannerVisible = false;
+  }
+}
