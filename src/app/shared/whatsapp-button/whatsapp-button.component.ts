b1f8ed95be963fcbd7e2778950437a8b2134967d
eric <ericmbouwe@yahoo.fr>
2024-06-30 23:09:30 +0100

    Ouverture directe  web ou mobile

--- src/app/shared/whatsapp-button/whatsapp-button.component.ts
+++ src/app/shared/whatsapp-button/whatsapp-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-whatsapp-button',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./whatsapp-button.component.css']
 })
 export class WhatsappButtonComponent {
+  mobileQuery: MediaQueryList;
+  message = "Bonjour, j'ai besoin de me faire former."
+  desktopLink = `https://web.whatsapp.com/send/?phone=33753428623&text=${this.message}`
+  mobileLink = `https://wa.me/33753428623?text=${this.message}`
 
+  constructor(
+    media: MediaMatcher,
+    changeDetectorRef: ChangeDetectorRef
+  ){
+    this.mobileQuery = media.matchMedia('(max-width: 1024px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  private _mobileQueryListener: () => void;
 }
