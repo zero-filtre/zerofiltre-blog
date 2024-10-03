e105d3a2d735415e0b8e08673f6cb95f44e0829e
eric <ericmbouwe@yahoo.fr>
2024-10-03 23:32:33 +0100

    Fix course card dropdown menu

--- src/app/shared/course-card/course-card.component.ts
+++ src/app/shared/course-card/course-card.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Course } from 'src/app/school/courses/course';
+import { CourseDeletePopupComponent } from 'src/app/school/courses/course-delete-popup/course-delete-popup.component';
+import { CourseService } from 'src/app/school/courses/course.service';
 import { AuthService } from 'src/app/user/auth.service';
+import { User } from 'src/app/user/user.model';
 
 @Component({
   selector: 'app-course-card',
@@ -14,10 +19,29 @@ export class CourseCardComponent {
 
   constructor(
     public authService: AuthService,
+    private courseService: CourseService,
+    private dialogDeleteRef: MatDialog,
+    private router: Router
+
   ) { }
 
   parseUrl(url:string) {
     return encodeURIComponent(url)
+  }
+
+  canEditCourse(course: Course) {
+    const user = this.authService?.currentUsr as User
+    return this.courseService.canEditCourse(user, course);
+  }
+
+  openCourseDeleteDialog(courseId: any): void {
+    this.dialogDeleteRef.open(CourseDeletePopupComponent, {
+      panelClass: 'delete-article-popup-panel',
+      data: {
+        courseId,
+        history: this.router.url
+      }
+    });
   }
 
 }
