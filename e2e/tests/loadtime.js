aa4827245312125985356b40ee8874f20dae6edd
eric <ericmbouwe@yahoo.fr>
2025-06-15 19:21:28 +0100

    Fix sonar lint v2

--- e2e/tests/loadtime.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'Load home page after 3 seconds max': (browser) => {
-        browser
-            .navigateTo('/')
-            .waitForElementVisible(".logo-link", 5000)
-            .assert.visible(".logo-link")
-    }
-}
