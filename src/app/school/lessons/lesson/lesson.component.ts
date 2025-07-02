470206b2951b9def5a6371f88c4f358ff89a1f0c
eric <ericmbouwe@yahoo.fr>
2025-07-02 11:55:30 +0100

    [REFACTO] Usage du companyId pour acceder aux infos du cours

--- src/app/school/lessons/lesson/lesson.component.ts
+++ src/app/school/lessons/lesson/lesson.component.ts
@@ -426,7 +426,7 @@ export class LessonComponent implements OnInit, OnDestroy {
       parseInt(res.url.split('/').pop() || '')
     );
 
-    const observables = ids.map((id) => this.courseService.findCourseById(id));
+    const observables = ids.map((id) => this.courseService.findCourseById(id, this.companyId));
 
     forkJoin(observables).subscribe({
       next: (courses: Course[]) => {
@@ -506,7 +506,7 @@ export class LessonComponent implements OnInit, OnDestroy {
 
   loadCourseData(courseId: any) {
     this.loadingCourse = true;
-    this.course$ = this.courseService.findCourseById(courseId).pipe(
+    this.course$ = this.courseService.findCourseById(courseId, this.companyId).pipe(
       catchError((err) => {
         this.loadingCourse = false;
         return throwError(() => err?.message);
